82081fb2055177150427febdd793482c1fde6718
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-12-08 22:30:01 +0500

    add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,19 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-  after_save :user_posts_count
-  after_destroy :user_posts_count
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  # A method which returns the 5 most recent comments for a given post.
+  after_create :increment_user_post_counter
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
-end
 
-private
-
-# A method that updates the posts count for a user.
-def user_posts_count
-  author.update(posts_counter: author.posts.count)
+  def increment_user_post_counter
+    author.increment!(:post_counter)
+  end
 end
