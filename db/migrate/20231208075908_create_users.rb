8091b1772289acabb0cd7a6e02bcf177da81f5fc
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-12-08 15:57:04 +0500

    define table schemas and relationships

--- db/migrate/20231208075908_create_users.rb
+++ db/migrate/20231208075908_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.photo :photo
+      t.string :photo
       t.text :bio
       t.integer :post_counter, default: 0
       t.timestamps
