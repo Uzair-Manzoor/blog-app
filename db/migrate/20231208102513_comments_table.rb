c65499216717809583ddcbd5b51b40b45e0f1c10
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-12-08 15:34:53 +0500

    create migration for comments table

---
+++ db/migrate/20231208102513_comments_table.rb
@@ -0,0 +1,11 @@
+class CommentsTable < ActiveRecord::Migration[7.1]
+  def change
+    create_table :comments do |t|
+      t.references :user, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: { to_table: :posts }
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
